83c644586c6dfe2f8e4c2739b91ed14d6529f151
vkopytov <vkopytov@sinergo.ru>
2016-02-12 11:19:04 +0300

    start_section fix

--- dist/formbuilder.js
+++ dist/formbuilder.js
@@ -812,7 +812,7 @@
 (function() {
   Formbuilder.registerField('section_start', {
     order: 99,
-    view: "<div class='section_start'>{</div>",
+    view: "",
     type: 'non_input',
     edit: "",
     addButton: "<span class='symbol'><span class='fa fa-minus'></span></span> Section Start"
